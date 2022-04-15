c3d43f7d69377cbe8a81f9b0e8004670e58b743d
sheyna-vlad <sheyna.bsuir@gmail.com>
2022-04-15 11:21:11 +0300

    newCase Great Work!

--- LadaOfficial/force-app/main/default/aura/newCase/newCaseHelper.js
+++ LadaOfficial/force-app/main/default/aura/newCase/newCaseHelper.js
@@ -1,22 +1,25 @@
 ({
   createCase: function(component, event) {
     var action = component.get("c.saveCase");
+    console.log(event);
     action.setParams({
-      case: event
+      caseJSON: event
+    });
+    console.log(action.getParams());
+
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      console.log(response.getReturnValue());
+      if (state === "SUCCESS" && response.getReturnValue() != NULL) {
+        console.log("New Case was created" + response.getReturnValue());
+      } else if (state === "ERROR") {
+        var errors = response.getError();
+        console.error(errors);
+        alert("Problem with connection." + errors);
+      }
     });
 
-//    action.setCallback(this, function(response) {
-//      var state = response.getState();
-//      if (state === "SUCCESS" && response.getReturnValue() != NULL) {
-//        alert("New Case was created" + response.getReturnValue().subject);
-//      } else if (state === "ERROR") {
-//        var errors = response.getError();
-//        console.error(errors);
-//        alert("Problem with connection." + errors);
-//      }
-//    });
-
     $A.enqueueAction(action);
-	System.debug('$A.enqueueAction(action);');
+    console.log("$A.enqueueAction(action);");
   }
 });
